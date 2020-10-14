fa76e2e4b1d413bd3ffd7a74c3fbed7391e9351b
ejelome <ejelome@gmail.com>
2020-10-14 20:35:39 +0200

    Update src/App.js for lesson 5.1

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { useReducer } from "react";
 
 import useLogger from "./useLogger";
 
-const initialContext = {};
-const CounterContext = createContext(initialContext);
-
-const CounterProvider = ({ children }) => {
-  const initialCount = 0;
-  const [count, setCount] = useState(initialCount);
-
-  const increment = () => setCount((prevCount) => prevCount + 1);
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-
-  const { Provider } = CounterContext;
-  const values = { count, increment, decrement };
-
-  return <Provider value={values}>{children}</Provider>;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
 };
 
 const Counter = () => {
-  const { count, increment, decrement } = useContext(CounterContext);
+  const initialState = 0;
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  const handleIncrement = () => dispatch({ type: "increment" });
+  const handleDecrement = () => dispatch({ type: "decrement" });
 
   useLogger(count);
 
   return (
     <>
       <h1>Counter</h1>
-      <button onClick={decrement}>-</button>
+      <button onClick={handleDecrement}>-</button>
       <code>{count}</code>
-      <button onClick={increment}>+</button>
+      <button onClick={handleIncrement}>+</button>
     </>
   );
 };
 
-const App = () => {
-  return (
-    <CounterProvider>
-      <Counter />
-    </CounterProvider>
-  );
-};
+const App = () => <Counter />;
 
 export default App;
